3cf2bd9fe946307e24d422bf1c69111b04f8152b
Ali Shiraee <a.shiraee@gmail.com>
2021-06-26 07:28:49 +0430

    rewrite pathing

--- segment.py
+++ segment.py
@@ -1,6 +1,7 @@
 from os import path
 import utility as ut
 from subprocess import call
+import shutil
 import argparse
 import cv2
 import os
@@ -15,39 +16,43 @@ parser.add_argument('--original', type=str, default='False', help='Preserve orig
 
 args = parser.parse_args()
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = path.dirname(os.path.realpath(__file__))
 INPUT_DIR = args.input
-FIRST_INPUT_DIR = os.path.join(BASE_DIR, INPUT_DIR)
-OUTPUT_DIR = os.path.join(BASE_DIR, args.output)
+FULL_INPUT_DIR = path.join(BASE_DIR, INPUT_DIR)
+OUTPUT_DIR = path.join(BASE_DIR, args.output)
 
 ut.create_directory(args.output)
 
 images_list = os.listdir(INPUT_DIR)
 _ , file_extension = os.path.splitext(images_list[0])
 
-if file_extension != '.jpg':
-    new_input = os.path.join(BASE_DIR, 'new_input')
+if file_extension not in ['.jpg', '.jpeg']:
+    new_input = path.join(BASE_DIR, 'input_jpg')
     ut.create_directory(new_input)
-    ut.convert_to_jpg(FIRST_INPUT_DIR, new_input)
-    NEW_INPUT_DIR = new_input
+    ut.convert_to_jpg(FULL_INPUT_DIR, new_input)
+    INPUT_DIR = new_input
 
-call(f'python test.py --trained_model={args.pretrained} --test_folder={NEW_INPUT_DIR} --cuda={args.cuda}', shell=True)
+call(f'python test.py --trained_model={args.pretrained} --test_folder={INPUT_DIR} --cuda={args.cuda}', shell=True)
 
 CACHE_FULL_DIR = os.path.join(BASE_DIR, 'result')
 
 for image in images_list:
-    image_name = image[:-4]
-    cv_img = cv2.imread(f'{FIRST_INPUT_DIR}/{image}')
-    out_dir = OUTPUT_DIR + '/' + image_name
-    ut.create_directory(out_dir)
-    if args.original == 'True':
-        call(f'cp {CACHE_FULL_DIR}/res_{image_name}.jpg {out_dir}', shell=True)
-    coordinates = ut.coordinates_reader(f'{CACHE_FULL_DIR}/res_{image_name}.txt')
-    segment_counter = 1
     print(f'Processing {image_name}')
+    
+    image_name, _ = path.splitext(image)
+    cv_img = cv2.imread(path.join(FULL_INPUT_DIR, image))
+    out_dir = os.path.join(OUTPUT_DIR, image_name)
+
+    ut.create_directory(out_dir)
+
+    if args.original == 'True':
+        shutil.copy(path.join(CACHE_FULL_DIR, f'res_{image_name}.jpg'), out_dir)
+
+    coordinates = ut.coordinates_reader(path.join(CACHE_FULL_DIR, f'res_{image_name}.txt'))
+    segment_counter = 1
     for coord in coordinates:
         segmented_image = ut.crop_image(cv_img, coord)
-        segmented_image_path = f'{out_dir}/{image_name}-cropped-{segment_counter}.'
+        segmented_image_path = path.join(out_dir, f'{image_name}-cropped-{segment_counter}.')
         if args.format == 'png':
             cv2.imwrite(segmented_image_path + 'png', segmented_image, [cv2.IMWRITE_PNG_COMPRESSION, 0])
         else:
@@ -56,5 +61,5 @@ for image in images_list:
         segment_counter += 1
 
 # Remove anything this code has created as cache
-call(f'rm -rf {BASE_DIR}/result', shell=True)
-call(f'rm -rf {BASE_DIR}/new_input', shell=True)
+shutil.rmtree(path.join(BASE_DIR, 'result'))
+shutil.rmtree(path.join(BASE_DIR, 'input_jpg'))
